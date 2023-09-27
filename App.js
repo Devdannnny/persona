14cdc7f142b288cd358947f5278731e3d950925b
Devdannnny <devdannny@gmail.com>
2023-09-27 16:19:47 +0000

    Latest Update

--- App.js
+++ App.js
@@ -1,11 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
+import { Environment, Inquiry } from "react-native-persona";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button
+        title="Start Inquiry"
+        onPress={() => {
+          Inquiry.fromTemplate("itmpl_DxG7ZJLsui8d8NrgnC1peHyK")
+            .environment(Environment.SANDBOX)
+            .onComplete((inquiryId, status, fields) =>
+              Alert.alert(
+                "Complete",
+                `Inquiry ${inquiryId} completed with status "${status}."`
+              )
+            )
+            .onCanceled((inquiryId, sessionToken) =>
+              Alert.alert("Canceled", `Inquiry ${inquiryId} was cancelled`)
+            )
+            .onError((error) => Alert.alert("Error", error.message))
+            .build()
+            .start();
+        }}
+      />
     </View>
   );
 }
@@ -13,8 +31,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
